ea63624c9732228d06a5d3bb3137e7b0518a67c8
Moratoe Mofokeng <ratoe16@gmail.com>
2019-03-19 16:15:28 +0200

    Fith Commit

--- testpackage/sorting.py
+++ testpackage/sorting.py
@@ -14,6 +14,22 @@ def bubble_sort(items):
 
 def merge_sort(items):
     '''Return array of items, sorted in ascending order'''
+def merge(A, B):
+    new_list = []
+    while len(A) > 0 and len(B) > 0:
+        if A[0] < B[0]:
+            new_list.append(A[0])
+            A.pop(0)
+        else:
+            new_list.append(B[0])
+            B.pop(0)
+
+    if len(A) == 0:
+        new_list = new_list + B
+    if len(B) == 0:
+        new_list = new_list + A
+
+    return new_list
     len_sort = len(items)
     # Base case. A list of size 1 is sorted.
 
