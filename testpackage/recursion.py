f33845bf91f3f60d7653c13f95a2e85eecbbd401
Moratoe Mofokeng <ratoe16@gmail.com>
2019-03-19 16:03:50 +0200

    Fourth Commit

--- testpackage/recursion.py
+++ testpackage/recursion.py
@@ -3,7 +3,7 @@ def sum_array(array):
     if len(array)==0:
         return 0
     else:
-        return array[0] + sum_array(arrayy[1:])
+        return array[0] + sum_array(array[1:])
 
 
 def fibonacci(n):
